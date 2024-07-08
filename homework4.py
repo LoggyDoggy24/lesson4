17220b8fdf2e0aaa7fa0ff5ad743f0d14486f970
LoggyDoggy24 <george199207@mail.ru>
2024-07-09 01:26:31 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+my_string = input("What is your full name? ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+my_string_new = print(my_string.replace(" ",""))
+get_first_char = my_string[0]
+get_last_char = my_string[-1]
+print(get_first_char)
+print(get_last_char)
